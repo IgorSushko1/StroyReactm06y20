2133d4fee589872733fdfcc65ee2338d0978c2b1
Igor Sushko <igor_sushko@yahoo.com>
2020-06-25 15:40:53 +0500

    Проверил работу автоустановщика
    По команде
    npm start -- --auto-install

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,12 @@
 import './index.css'
 
 import React from 'react'
-import {render} from 'react-dom'
+import {
+  render
+} from 'react-dom'
+
+import Rx from 'rx'
 
 import App from './App'
 
-render(<App/>, document.querySelector('#app'))
+render( < App / > , document.querySelector('#app'))
