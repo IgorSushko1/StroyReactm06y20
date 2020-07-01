aa6e4e31ffa57430780eeea965bac27c3bbfc63e
Igor Sushko <igor_sushko@yahoo.com>
2020-07-01 11:57:19 +0500

    Сборка работает с jsx файлами добавил правила с issues

--- client/nwb.config.js
+++ client/nwb.config.js
@@ -1,3 +1,20 @@
+const {
+  resolve
+} = require('path');
+
 module.exports = {
-  type: 'react-app'
-}
+  type: 'react-app',
+  webpack: {
+    rules: {
+      babel: {
+        test: /\.jsx?/,
+      },
+    },
+    extra: {
+      entry: resolve(__dirname, 'src'),
+      resolve: {
+        extensions: ['.js', '.jsx'],
+      },
+    },
+  },
+};
