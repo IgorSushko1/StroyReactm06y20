d7298a5ff8fb11c00bbad85e952b4a38d407d30b
Igor Sushko <igor_sushko@yahoo.com>
2020-07-02 00:17:42 +0500

    По уроку DragStart and Update

--- client/src/lessonsComponents/AppLessons.jsx
+++ client/src/lessonsComponents/AppLessons.jsx
@@ -11,7 +11,23 @@ import Column from './Column.jsx';
 class App extends Component {
   state = initialData;
 
+  onDragStart = () => {
+    document.body.style.color = 'orange';
+    document.body.style.transition = 'background-color 0.2s ease';
+  };
+
+  onDragUpdate = (update) => {
+    const { destination } = update;
+    const opacity = destination
+      ? destination.index / Object.keys(this.state.tasks).length
+      : 0;
+    document.body.style.backgroundColor = `rgba(153, 141, 217, ${opacity})`;
+  };
+
   onDragEnd = (result) => {
+    document.body.style.color = 'inherit';
+    document.body.style.backgroundColor = 'inherit';
+
     const { destination, source, draggableId } = result;
 
     if (!destination) {
@@ -60,7 +76,11 @@ class App extends Component {
   a = {};
   render() {
     return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
+      <DragDropContext
+        onDragStart={this.onDragStart}
+        onDragUpdate={this.onDragUpdate}
+        onDragEnd={this.onDragEnd}
+      >
         {this.state.columnOrder.map((columnId) => {
           const column = this.state.columns[columnId];
           const tasks = column.taskIds.map(
